28ab0cef4c53f9080fc8b9334d6c99fdef4bffee
Tim <ttolk89@gmail.com>
2023-02-07 13:44:58 +0100

    New design (#2)
    * new design
    
    * new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
 import './App.scss'
+import './styles/Navbar.scss'
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import * as React from "react";
 import Home from "./components/Home";
 import Work from "./components/Work";
-import Container from "@mui/material/Container";
 
 
 function App() {
 
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-
     return (
         <div className="App">
-
             <div className='wrapperContent'>
-                <Container maxWidth="xl">
 
-                    <Home/>
+                <Navbar/>
+                <Home/>
+                <Work/>
 
-                    <div className='break'></div>
-
-                    <Work/>
-
-                    <div className='break'></div>
-
-                    <Footer/>
-                </Container>
             </div>
-
         </div>
     )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import {Grid, Paper, styled} from "@mui/material";
 import Box from "@mui/material/Box";
 import '../App.scss'
-import avatar from '../images/avatar.png'
+import '../styles/navbar.scss'
 import * as React from "react";
 
 
@@ -17,92 +17,25 @@ function Home() {
     return (
 
         <div className={'Startseite'}>
-            <Box sx={{
-                flexGrow: 1,
-                paddingBottom: 10,
-                height: 'auto'
-            }}>
-
-                <div className='wrapperTitle'>
 
 
-                    <div className='wrapperHead'>
-                        <h1 className='firstTitle'>Hallo</h1>
-                        <h2 className='secondTitle'>Ich bin Tim</h2>
-                        <h2 className='secondTitle underTitle'>Webentwickler</h2>
-                        <a href='https://www.linkedin.com/in/tim-tolk-2091a7258/' target='_blank'>
-                            <button className='headerBtn'>Besuche mich auf LinkedIn</button>
-                        </a>
-                    </div>
-
-
-                    <img src={avatar} alt='Mann mit Brille, kurzen Haaren und Bart'></img>
-
-
+            <div className='wrapperHead'>
+                <img className={'bgImage'} src={'../public/images/bg.jpg'}/>
+                <div className={'wrapperHeadContent'}>
+                    <h1 className='headTitle'>Web-Entwicklung in Berlin</h1>
+                    <h2 className='title'>Ich bin ein Entwickler aus Berlin. Ich biete
+                        qualitativ hochwertige Dienstleistungen in der Webentwicklung und haben mich auf die
+                        Bereiche
+                        Custom Web Development, Backend und Python spezialisiert. Ich freue mich darauf, Ihnen zu
+                        helfen und Ihre Ideen zum Leben zu erwecken.</h2>
+                    <a href='https://www.linkedin.com/in/tim-tolk-2091a7258/' target='_blank'>
+                        <button className='headerBtn'>Kontakt</button>
+                    </a>
                 </div>
+            </div>
 
 
-                <div className='break'></div>
-
-                <p className='numberOfYear'>#16</p>
-                <p className='years'>Jahre Arbeitserfahrung</p>
-
-                <Grid container spacing={3}>
-                    <Grid item xs>
-                        <Item>
-                            <div className='boxExp'>
-                                <h2 className='mainExp'>2022</h2>
-                                <h3 className='subExp'>Weiterbildung Fullstack Entwickler</h3>
-                                <article>
-                                    Im Jahr 2022 habe ich mich für den Weg der Webentwicklung entschieden und meine
-                                    Weiterbildung beim DCI erfolgreich abgeschlossen und mein Wissen stark verbessert
-                                    und vertieft.
-                                </article>
-                            </div>
-                        </Item>
-                    </Grid>
-                    <Grid item xs>
-                        <Item sx={{paddingTop: 10}}>
-                            <div className='boxExp'>
-                                <h2 className='mainExp'>2021</h2>
-                                <h3 className='subExp'>Lernphase Programmierung</h3>
-                                <article>
-                                    Da ich ein starkes Interesse an der Softwareentwicklung habe und der Bereich der
-                                    Sicherheit und Anwendungsprogrammierung eine große Rolle spielt. Habe ich mich
-                                    intensiv mit Python beschäftigt.
-                                </article>
-                            </div>
-                        </Item>
-                    </Grid>
-                    <Grid item xs>
-                        <Item>
-                            <div className='boxExp'>
-                                <h2 className='mainExp'>2020</h2>
-                                <h3 className='subExp'>Lernphase Programmierung</h3>
-                                <article>
-                                    Jeder Anfang ist schwer! Daher habe ich mich ein Jahr lang mit Java und der OOP
-                                    Programmierung beschäftigt.
-                                    Und meine Kenntnisse in der Anwendungsprogrammierung verbessert.
-                                </article>
-                            </div>
-                        </Item>
-                    </Grid>
-                    <Grid item xs>
-                        <Item sx={{paddingTop: 10}}>
-                            <div className='boxExp'>
-                                <h2 className='mainExp'>2006 - 2019</h2>
-                                <h3 className='subExp'>Ausbildung und Berufserfahrung</h3>
-                                <article>
-                                    Da ich vielseitig interessiert bin, wollte ich zunächst mein wirtschaftliches Wissen
-                                    beruflich vertiefen. Daher habe ich mich entschieden, eine
-                                    Ausbildung im Einzelhandel zu machen. Anschließend spezialisierte ich mich auf die
-                                    Automatisierungstechnik, dort habe ich Blueprints mit CAD programmiert.
-                                </article>
-                            </div>
-                        </Item>
-                    </Grid>
-                </Grid>
-            </Box></div>
+        </div>
     )
 }
 
